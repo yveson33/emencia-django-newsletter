36c397b22373a90cf91421d25d5960508ed17914
Mathieu Comandon <strycore@gmail.com>
2012-07-25 17:59:01 +0200

    make mailing_list_id on newsletter nullable

--- emencia/django/newsletter/models.py
+++ emencia/django/newsletter/models.py
@@ -219,7 +219,8 @@ class Newsletter(models.Model):
     content = models.TextField(_('content'), help_text=_('Or paste an URL.'),
                                default=_('<body>\n<!-- Edit your newsletter here -->\n</body>'))
 
-    mailing_list = models.ForeignKey(MailingList, verbose_name=_('mailing list'))
+    mailing_list = models.ForeignKey(MailingList,
+                                     verbose_name=_('mailing list'), null=True)
     test_contacts = models.ManyToManyField(Contact, verbose_name=_('test contacts'),
                                            blank=True, null=True)
 
